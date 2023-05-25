235a295e7755afda942f9ae4205af4a0a1b8f7ba
Idaraobong James <iydeejaymes@gmail.com>
2023-05-25 03:13:32 +0100

    updates

--- sctipt.js
+++ sctipt.js
@@ -1,3 +1,29 @@
+// Pages
+document.querySelectorAll('.logo').forEach(logo => {
+    logo.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'block'
+        document.querySelector('.login-page').style.display = 'none'
+        document.querySelector('.signup-page').style.display = 'none'
+    })
+})
+
+document.querySelectorAll('.login').forEach(loginBtn => {
+    loginBtn.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'none'
+        document.querySelector('.login-page').style.display = 'block'
+        document.querySelector('.signup-page').style.display = 'none'
+    })
+})
+
+document.querySelectorAll('.signup').forEach(signupBtn => {
+    signupBtn.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'none'
+        document.querySelector('.login-page').style.display = 'none'
+        document.querySelector('.signup-page').style.display = 'flex'
+    })
+})
+// End of Pages
+
 // Navigation
 const dropdownItems = document.querySelectorAll('.dropdown-hover')
 
@@ -25,6 +51,19 @@ if(window.innerHeight < 1000) {
         headingLink.addEventListener('click', () => {
         headingLink.parentElement.parentElement.style.left = '-20rem'
     })
+    })
+}else {
+    dropdownItems.forEach(dropdownItem => {
+        dropdownItem.addEventListener('mouseover', () => {
+            dropdownItem.lastElementChild.style.cssText = 'opacity: 1; visibility: visible'
+            document.querySelector('.navbar-wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
+            dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(180deg)'
+        })
+        dropdownItem.addEventListener('mouseout', () => {
+            dropdownItem.lastElementChild.style.cssText = 'opacity: 0; visibility: hidden'
+            document.querySelector('.navbar-wrapper').style.background = 'none'
+            dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(0)'
+        })
     })
 }
 // End of Navigation
